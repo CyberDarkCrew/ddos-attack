a84ec4f5344a255c64c0ca6c9f966f43ae84c969
CyberDarkCrew <Cyb3rD4rk@protonmail.com>
2021-12-07 09:49:00 +0200

    Add files via upload

---
+++ start.py
@@ -0,0 +1,51 @@
+from os import *
+import os
+from time import sleep
+
+# = = = = = = = = = = = = 
+BRed="\033[1;31m" # Red
+BGreen="\033[1;32m" # Green
+BYellow="\033[1;33m" # Yellow
+BBlue="\033[1;34m" # Blue
+BPurple="\033[1;35m" # Purple
+BCyan="\033[1;36m" # Cyan
+BWhite="\033[1;37m" # White
+Z = '\033[1;31m' #احمر
+A = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+X = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+S = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+# = = = = = = = = = = = =
+
+os.system('clear')
+print(S+ '  ____      _                 ____             _ ')
+print(S+ ' / ___|   _| |__   ___ _ __  |  _ \  __ _ _ __| | __')
+print(S+ '''| |  | | | | '_ \ / _ \ '__| | | | |/ _` | '__| |/ / ''')
+print(S+ '| |__| |_| | |_) |  __/ |    | |_| | (_| | |  |   < ')
+print(S+ ' \____\__, |_.__/ \___|_|    |____/ \__,_|_|  |_|\_\ ', Z+'DDOS ATTACK v1.0')
+print(S+ '      |___/                                  ', Z+'~ Upgrade Tool By Cyber Dark ~')
+
+print('')
+
+print(A+'~ Code By Cyber Dark')
+print(A+'~ Facebook : @c0.dark')
+print(A+'~ TikTok   : @cyb3r_dark')
+print(A+'~ Youtube  : Cyber Dark')
+
+
+print(F+'''
+~ Cyber Dark Team ~
+Telegram : 
+	Mahmoud : @L_N_X_0
+	Ahmed   : @M3x4il
+	Hosin   : @Hosinballo
+	Salem   : @k_J_I_A
+	Omer    : @nice_nice
+ ''')
+print('')
+sleep(0.9)
+
+os.system('python3 attack.py -s 185.34.219.123')
